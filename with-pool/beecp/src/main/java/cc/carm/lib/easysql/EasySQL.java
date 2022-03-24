ebe51dd9a3f5116b51ebe74c792e69dee410b9d2
CarmJos <KarmunJ@outlook.com>
2022-03-25 00:41:36 +0800

    改用Format形式

--- with-pool/beecp/src/main/java/cc/carm/lib/easysql/EasySQL.java
+++ with-pool/beecp/src/main/java/cc/carm/lib/easysql/EasySQL.java
@@ -4,48 +4,38 @@ import cc.carm.lib.easysql.api.SQLManager;
 import cc.carm.lib.easysql.api.SQLQuery;
 import cc.carm.lib.easysql.api.util.TimeDateUtils;
 import cc.carm.lib.easysql.manager.SQLManagerImpl;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import cn.beecp.BeeDataSource;
+import cn.beecp.BeeDataSourceConfig;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Properties;
 
 public class EasySQL {
 
     public static SQLManagerImpl createManager(
             @NotNull String driver, @NotNull String url,
             @NotNull String username, @Nullable String password) {
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName(driver);
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
-        return createManager(config);
+        return createManager(new BeeDataSourceConfig(driver, url, username, password));
     }
 
-    public static SQLManagerImpl createManager(@NotNull Properties properties) {
-        return createManager(new HikariConfig(properties));
+    public static SQLManagerImpl createManager(@NotNull BeeDataSourceConfig config) {
+        return new SQLManagerImpl(new BeeDataSource(config));
     }
 
-    public static SQLManagerImpl createManager(@NotNull HikariConfig config) {
-        return new SQLManagerImpl(new HikariDataSource(config));
-    }
 
     public static void shutdownManager(SQLManager manager, boolean forceClose, boolean outputActiveQuery) {
         if (!manager.getActiveQuery().isEmpty()) {
             manager.getLogger().severe("There are " + manager.getActiveQuery().size() + " connections still running");
             for (SQLQuery value : manager.getActiveQuery().values()) {
                 if (outputActiveQuery) {
-                    manager.getLogger().severe("#" + value.getAction().getShortID() + " -> " + value.getSQLContent());
-                    manager.getLogger().severe("- execute at " + TimeDateUtils.getTimeString(value.getExecuteTime()));
+                    manager.getLogger().severe(String.format("#%s -> %s", value.getAction().getShortID(), value.getSQLContent()));
+                    manager.getLogger().severe(String.format("- execute at %s", TimeDateUtils.getTimeString(value.getExecuteTime())));
                 }
                 if (forceClose) value.close();
             }
         }
-        if (manager.getDataSource() instanceof HikariDataSource) {
-            //Close hikari pool
-            ((HikariDataSource) manager.getDataSource()).close();
+        if (manager.getDataSource() instanceof BeeDataSource) {
+            //Close bee connection pool
+            ((BeeDataSource) manager.getDataSource()).close();
         }
     }
 
